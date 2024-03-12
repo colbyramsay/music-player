a90eb4d826153e1f0409dad0f130fe8b70a0fa68
Colby Ramsay <149699778+colbyramsay@users.noreply.github.com>
2024-03-11 22:38:16 -0700

    Display songs in the DOM using innerHTML

--- script.js
+++ script.js
@@ -4,23 +4,37 @@ const allSongs = [
     title: "boots of spanish leather",
     artist: "bob dylan",
     duration: "1:11",
-    src: "./songs/test.mp3"
+    src: "./songs/test.mp3",
   },
   {
     id: 1,
     title: "girl from the north country",
     artist: "bob dylan",
     duration: "2:22",
-    src: "./songs/test.mp3"
+    src: "./songs/test.mp3",
   },
   {
     id: 3,
     title: "no woman no cry",
     artist: "bob marley",
     duration: "3:33",
-    src: "./songs/test.mp3"
+    src: "./songs/test.mp3",
   },
-]
+  {
+    id: 4,
+    title: "the gambler",
+    artist: "kenny rogers",
+    duration: "4:44",
+    src: "./songs/test.mp3",
+  },
+  {
+    id: 5,
+    title: "another one bites the dust",
+    artist: "queen",
+    duration: "5:55",
+    src: "./songs/test.mp3",
+  },
+];
 
 const prevBtn = document.getElementById("previous");
 const playBtn = document.getElementById("play");
@@ -37,3 +51,12 @@ let currentSongs = {
 };
 
 console.log(currentSongs);
+
+const displaySongs = (array) => {
+  const songsHTML = array.map((song) => {
+    return `<li>${song.title}</li>`;
+  }).join("");
+  playlist.innerHTML = songsHTML;
+};
+
+displaySongs(currentSongs?.songs);
